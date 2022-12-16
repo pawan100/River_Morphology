99bdf82734978b5b82719a71eccb78d287c8aece
Pawan Thapa <pawanthapa42@gmail.com>
2022-12-15 20:10:58 -0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,10 @@
+# import libraries
+#pip install pystac-client sat-search 
+from pystac_client import Client
+
+LandsatSTAC = Client.open("https://landsatlook.usgs.gov/stac-server", headers=[])
+
+for collection in LandsatSTAC.get_collections():
+    print(collection)
+# functions
+# new
