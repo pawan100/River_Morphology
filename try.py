4ae64f85b304a1aba60f7b23602e153d3c470060
Pawan100 <pawanthapa42@gmail.com>
2022-12-27 18:03:33 -0600

    try

--- try.py
+++ try.py
@@ -18,6 +18,9 @@ band3 = rasterio.open('D:\OneDrive - The University of Alabama\Paper\PCD\Satelli
 band6 = rasterio.open('D:\OneDrive - The University of Alabama\Paper\PCD\Satelliteimage\Landsat8_2022/LC08_L2SP_021039_20211201_20211209_02_T1_SR_B6.tif') #swir
 
 #sentinel 2
+band3S = rasterio.open('D:\OneDrive - The University of Alabama\Paper\PCD\Satelliteimage\T16RDV_20221213T163709_B03_10m') #red
+band9 = rasterio.open('D:\OneDrive - The University of Alabama\Paper\PCD\Satelliteimage\S2B_MSIL2A_20221213T163709_N0509_R083_T16RDV_20221213T190508.SAFE\GRANULE\L2A_T16RDV_A030139_20221213T164134\IMG_DATA\R60m\T16RDV_20221213T163709_B09_60m.tif') #swir
+
 
 # clip study area mobile river
 
@@ -28,5 +31,7 @@ band6 = rasterio.open('D:\OneDrive - The University of Alabama\Paper\PCD\Satelli
 # median filter
 
 #plot band 
-plot.show(band3)
+#plot.show(band3)
+plot.show (band3S)
 #ndwi
+#try
